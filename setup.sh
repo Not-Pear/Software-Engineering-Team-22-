7b4ee6aeb7f54d0b318486736ec3303b8b0fe9f3
MiloPumford <ampumford@gmail.com>
2025-03-21 12:59:43 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#chmod 755 setup.sh
+
+sudo apt update
+sudo apt install python3
+sudo apt install python3-pip
+pip3 install tk
+pip3 instlal pillow
+pip3 install psycopg2-binary
+sudo apt install python3-tk 
